b99ba5800f4942f3df1b9fd652ccfea54e30b4cf
Soc Sieng <socsieng@gmail.com>
2019-11-20 11:20:17 -0800

    Add configuration to publish npm package

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,28 @@
 import typescript from 'rollup-plugin-typescript2';
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/GooglePayButton.js',
+      format: 'cjs'
+    },
+    external: [
+      'react'
+    ],
+    plugins: [
+      typescript()
+    ],
   },
-  external: [
-    'react'
-  ],
-  plugins: [
-    typescript()
-  ]
-};
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/GooglePayButton.d.ts',
+      format: 'cjs'
+    },
+    plugins: [
+      dts()
+    ],
+  },
+];

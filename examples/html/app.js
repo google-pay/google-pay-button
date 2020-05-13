3ca68b7b7a09a8e2f8e9e0ec700a927381c4c365
Soc Sieng <socsieng@google.com>
2020-05-12 23:27:00 -0700

    Update html example

---
+++ examples/html/app.js
@@ -0,0 +1,250 @@
+import GooglePayButton from './node_modules/@google-pay/button-element/dist/index.js';
+
+class ControlAccessor {
+  constructor(id) {
+    this.id = id;
+  }
+
+  get control() {
+    return document.getElementById(this.id);
+  }
+
+  get value() {
+    return this.control.value;
+  }
+}
+
+const controls = {
+  amount: new ControlAccessor('amount'),
+  existingPaymentRequired: new ControlAccessor('existing-payment-method-required'),
+  buttonColor: new ControlAccessor('button-color'),
+  buttonType: new ControlAccessor('button-type'),
+};
+
+const defaultPaymentRequest = {
+  apiVersion: 2,
+  apiVersionMinor: 0,
+  allowedPaymentMethods: [
+    {
+      type: 'CARD',
+      parameters: {
+        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+        allowedCardNetworks: ['MASTERCARD', 'VISA'],
+        billingAddressParameters: {
+          format: 'MIN',
+        },
+      },
+      tokenizationSpecification: {
+        type: 'PAYMENT_GATEWAY',
+        parameters: {
+          'gateway': 'stripe',
+          'stripe:version': '2018-10-31',
+          'stripe:publishableKey': 'pk_test_MNKMwKAvgdo2yKOhIeCOE6MZ00yS3mWShu',
+        },
+      },
+    },
+  ],
+  merchantInfo: {
+    merchantId: '17613812255336763067',
+    merchantName: 'Demo Merchant',
+  },
+  transactionInfo: {
+    totalPriceStatus: 'FINAL',
+    totalPriceLabel: 'Total',
+    totalPrice: '100.00',
+    currencyCode: 'USD',
+    countryCode: 'US',
+  },
+};
+
+function onLoadPaymentData(paymentData) {
+  console.log('load payment data', paymentData);
+}
+
+function onPaymentAuthorized(paymentData) {
+  return {
+    transactionState: 'SUCCESS',
+  };
+}
+
+const googlePayButtons = [
+  {
+    title: 'Basic Example',
+    get props() {
+      return {
+        buttonColor: controls.buttonColor.value,
+        buttonType: controls.buttonType.value,
+        paymentRequest: {
+          ...defaultPaymentRequest,
+          transactionInfo: {
+            ...defaultPaymentRequest.transactionInfo,
+            totalPrice: controls.amount.value,
+          },
+        },
+        existingPaymentRequired: controls.existingPaymentRequired.value === 'true',
+        onLoadPaymentData,
+      };
+    },
+  },
+  {
+    title: 'Cryptogram 3ds',
+    get props() {
+      return {
+        buttonColor: controls.buttonColor.value,
+        buttonType: controls.buttonType.value,
+        paymentRequest: {
+          ...defaultPaymentRequest,
+          allowedPaymentMethods: [
+            {
+              type: 'CARD',
+              parameters: {
+                allowedAuthMethods: ['CRYPTOGRAM_3DS'],
+                allowedCardNetworks: ['AMEX', 'VISA', 'MASTERCARD'],
+              },
+              tokenizationSpecification: {
+                type: 'PAYMENT_GATEWAY',
+                parameters: {
+                  'gateway': 'stripe',
+                },
+              },
+            },
+          ],
+          transactionInfo: {
+            ...defaultPaymentRequest.transactionInfo,
+            totalPrice: controls.amount.value,
+          },
+        },
+        existingPaymentRequired: controls.existingPaymentRequired.value === 'true',
+        onLoadPaymentData,
+        onPaymentAuthorized,
+      };
+    },
+  },
+  {
+    title: 'Payment Authorization',
+    get props() {
+      return {
+        buttonColor: controls.buttonColor.value,
+        buttonType: controls.buttonType.value,
+        paymentRequest: {
+          ...defaultPaymentRequest,
+          transactionInfo: {
+            ...defaultPaymentRequest.transactionInfo,
+            totalPrice: controls.amount.value,
+          },
+        },
+        existingPaymentRequired: controls.existingPaymentRequired.value === 'true',
+        onLoadPaymentData,
+        onPaymentAuthorized,
+      };
+    },
+  },
+  {
+    title: 'Require Shipping',
+    get props() {
+      return {
+        buttonColor: controls.buttonColor.value,
+        buttonType: controls.buttonType.value,
+        paymentRequest: {
+          ...defaultPaymentRequest,
+          transactionInfo: {
+            ...defaultPaymentRequest.transactionInfo,
+            totalPrice: controls.amount.value,
+          },
+        },
+        existingPaymentRequired: controls.existingPaymentRequired.value === 'true',
+        shippingAddressRequired: true,
+        onLoadPaymentData,
+        onPaymentAuthorized,
+      };
+    },
+  },
+];
+
+window.controls = controls;
+
+function initializeControls() {
+  Object.values(controls).forEach(props => {
+    props.control.addEventListener('change', () => {
+      updateGooglePayButtons();
+    });
+  });
+}
+
+function updateGooglePayButtons() {
+  googlePayButtons.forEach((gpay, index) => {
+    const id = `button${index}`;
+    let button = document.getElementById(id);
+
+    if (!button) {
+      button = new GooglePayButton();
+      button.id = id;
+
+      const container = document.getElementById('examples');
+      container.appendChild(createExample(gpay.title, button));
+    }
+
+    Object.assign(button, gpay.props);
+  });
+}
+
+function createExample(text, button) {
+  const example = document.createElement('div');
+  example.className = 'example';
+  const title = document.createElement('div');
+  title.className = 'title';
+  const demo = document.createElement('div');
+  demo.className = 'demo';
+
+  title.innerText = text;
+
+  demo.appendChild(button);
+
+  example.appendChild(title);
+  example.appendChild(demo);
+
+  return example;
+}
+
+initializeControls();
+updateGooglePayButtons();
+
+// create the button
+const staticButton = document.getElementById('static');
+
+staticButton.paymentRequest = {
+  apiVersion: 2,
+  apiVersionMinor: 0,
+  allowedPaymentMethods: [
+    {
+      type: 'CARD',
+      parameters: {
+        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+        allowedCardNetworks: ['MASTERCARD', 'VISA'],
+        billingAddressParameters: {
+          format: 'MIN',
+        },
+      },
+      tokenizationSpecification: {
+        type: 'PAYMENT_GATEWAY',
+        parameters: {
+          'gateway': 'stripe',
+          'stripe:version': '2018-10-31',
+          'stripe:publishableKey': 'pk_test_MNKMwKAvgdo2yKOhIeCOE6MZ00yS3mWShu',
+        },
+      },
+    },
+  ],
+  merchantInfo: {
+    merchantId: '17613812255336763067',
+    merchantName: 'Demo Merchant',
+  },
+  transactionInfo: {
+    totalPriceStatus: 'FINAL',
+    totalPriceLabel: 'Total',
+    totalPrice: '100.00',
+    currencyCode: 'USD',
+    countryCode: 'US',
+  },
+};
+staticButton.onLoadPaymentData = onLoadPaymentData;

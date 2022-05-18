77ac315205b80c9d2a70c507763ab867a10575b2
shirly-chen-awx <shirly.chen@airwallex.com>
2022-05-18 20:53:42 +0800

    fix: remove history in gitignore and keep the if clause outside of the try block

--- src/lib/button-manager.ts
+++ src/lib/button-manager.ts
@@ -76,17 +76,18 @@ export class ButtonManager {
   }
 
   async mount(element: Element): Promise<void> {
-    try {
-      if (!this.isGooglePayLoaded()) {
+    if (!this.isGooglePayLoaded()) {
+      try {
         await loadScript('https://pay.google.com/gp/p/js/pay.js');
-      }
-    } catch(err) {
-      if (this.config?.onError) {
-        this.config.onError(err as Error);
-      } else {
-        console.error(err);
+      } catch(err) {
+        if (this.config?.onError) {
+          this.config.onError(err as Error);
+        } else {
+          console.error(err);
+        }
       }
     }
+
   
 
     this.element = element;

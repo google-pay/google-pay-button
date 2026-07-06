c5317822327251e79bfec2f36d809842ea40b31c
Dominik Mengelt <dmengelt@google.com>
2026-07-06 10:34:28 +0200

    feat!(angular): upgrade library and example app to Angular 22 (#420)
    Upgrade the Angular component library and its example application to Angular 22. Bumps peer dependencies to drop support for older versions of Angular.

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -14,13 +14,15 @@
  * limitations under the License.
  */
 
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ReadyToPayChangeResponse } from '@google-pay/button-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class AppComponent {
   amount = '100.00';
